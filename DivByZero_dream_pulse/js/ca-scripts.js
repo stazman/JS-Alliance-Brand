fa4c20121ac7b1d0c1762de209d754b08cd5b362
Christopher DiStasio <cadistasio@gmail.com>
2022-10-07 18:57:56 -0500

    Gives partial solution

--- DivByZero_dream_pulse/js/ca-scripts.js
+++ DivByZero_dream_pulse/js/ca-scripts.js
@@ -1,25 +1,35 @@
 var customerName = "Alliance  ";
 
 // <h1 id="tm-header" class="tm-brand text-uppercase"></h1> <!--todo: Add Customer Name-->
-function addCustomerName() {
+function addCustomerName(customerName) {
 
     let title = document.getElementById('title')
     title.textContent = customerName;
-    
-    // <h1 id="tm-header" class="tm-brand text-uppercase"></h1> <!--todo: Add Customer Name-->
 
-    // <h2 id="introheader" class="tm-section-title mb-5 text-uppercase tm-color-primary"></h2> <!--todo: Add Customer Name-->
+    let header1 = document.getElementById('tm-header')
+
+    header1.textContent = customerName
+
+    let h2 = document.getElementById('introheader')
+
+    h2.textContent = customerName
+
 
 };
 
 function toggleColors() {
+    let toggClrs = document.getElementsByClassName("tm-white-rect");
+
+    toggClrs.className = "tm-blue-rect";
     // <!-- todo: Change all classes named 'tm-white-rect' to 'tm-blue-rect' -->
 
 };
 
 function hideElement() {
-    //<ul class="nav flex-row tm-social-links">  <!--todo: Hide social links-->
 
+    let hidEle = document.querySelectorAll(".tm-social-links")//<ul class="nav flex-row tm-social-links">  <!--todo: Hide social links-->
+
+    hidEle.display = "none"
 };
 
  function addText() {
@@ -34,7 +44,9 @@ function hideElement() {
 
     // <p id="addresspara" class="mb-3"> <!--todo: Add customer company phone number text-->
 
-}; 
+};
 
 
-
+addCustomerName(customerName)
+toggleColors()
+hideElement()
